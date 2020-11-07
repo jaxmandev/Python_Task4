862ee836c9db324d23a440b4a85169d7782ac6c1
Jax <main@Jaxs-MacBook-Pro.local>
2020-11-07 23:20:30 +0000

    Added phase1.py solution and README.md

---
+++ phase1.py
@@ -0,0 +1,34 @@
+
+class Operations:
+    def __init__(self):
+
+        # create an addition function
+        def add(self, num1, num2):
+            return num1 + num2
+        
+        # create a subtraction function
+        def subtract(self, arg1, arg2):
+            return arg1 - arg2
+        
+        # create a multiplication function
+        def multiply(self, arg1, arg2):
+            return arg1 * arg2
+        
+        # create a division function
+        def divide(self, arg1, arg2):
+            return arg1 / arg2
+        
+        # create a percentile function
+        def percent(self, arg1, arg2):
+            percent = (arg1 / arg2) * 100
+            return percent
+        
+        # create an exponentiation function
+        def power(self, arg1, arg2):
+            return arg1**arg2
+        
+        # create a modulus function
+        def modulus(self, arg1, arg2):
+            return arg1 % arg2
+
+
